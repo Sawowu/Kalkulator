b57a4a495c5899a024b8ac2e08f97e08b401c017
PC <PC@DESKTOP-RK8HSQO>
2023-02-19 18:36:06 +0100

    Add project files.

---
+++ Kalk/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Library;
+
+char[] symbols = { '(', ')', '^', '*', '/', '+', '-' };
+
+double solve(string eq)
+{
+    bool found = false;
+    int startIndex = 0;
+
+    for(int i = 0; i<symbols.Length; )
+    {
+        for(int j = 0; j<eq.Length; j++ )
+        {
+            if (i == 0)
+            {
+                if (eq[j] == '(')
+                {
+                    found = true;
+                    startIndex = j; break;
+                }
+                if (j == eq.Length-1 && eq[j] != symbols[j] && !found) i=2;
+            }
+            else if(i==1) {
+                if (eq[j] ==')') {
+                    eq.Replace(eq.Remove(j).Remove(0, startIndex+1), Convert.ToString(solve(eq.Remove(j).Remove(0, startIndex + 1))));
+                    i = 0; break;
+                }
+            }
+            else if(1==2)
+            {
+
+            }
+        }
+        if (found)
+        {
+            found = false;
+            i++;
+        }
+    }
+    return 0;
+}
