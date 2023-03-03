22723218e07b9a8f371289b82c83e658116db109
Sawowu <bk11czerwiec@gmail.com>
2023-03-03 11:36:50 +0100

    Testy

--- Testy/TestRowKwadrat.cs
+++ Testy/TestRowKwadrat.cs
@@ -11,5 +11,17 @@ namespace Testy
             kw = new Kwadratowe(1, -2, 1);
             Assert.Equal(new double[] { 1 }, kw.Rozwiazania());
         }
+        [Fact]
+        public void Test2()
+        {
+            kw = new Kwadratowe(2, -5, -3);
+            Assert.Equal(new double[] {3, -0.5}, kw.Rozwiazania());
+        }
+        [Fact]
+        public void Test3() 
+        {
+            kw = new Kwadratowe(4, 3, 2);
+            Assert.Equal(null, kw.Rozwiazania());
+        }
     }
 }
---
+++ Testy/TestTrapez.cs
@@ -0,0 +1,20 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testy
+{
+    public class TestTrapez
+    {
+        private Trapez tr;
+        [Fact]
+        public void Test()
+        {
+            tr = new Trapez(4, 2, 1.5, 1);
+            Assert.Equal(3, tr.Pole());
+        }
+    }
+}
